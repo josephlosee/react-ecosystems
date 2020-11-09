cbfeb9d55940396bf258e8c29b05ffe1923d6f16
Joseph Losee <josephlosee@gmail.com>
2020-11-09 10:39:00 -0800

    Part 4-6: Added Load Actions for thunk

--- src/Todos/actions.js
+++ src/Todos/actions.js
@@ -1,9 +1,11 @@
 export const CREATE_TODO = 'CREATE_TODO';
 export const REMOVE_TODO = 'REMOVE_TODO';
 export const MARK_TODO_AS_COMPLETED = 'TODO_COMPLETED';
+export const LOAD_TODOS_IN_PROGRESS = 'LOAD_TODOS_IN_PROGRESS';
+export const LOAD_TODOS_SUCCESS = 'LOAD_TODOS_SUCCESS';
+export const LOAD_TODOS_FAILURE = 'LOAD_TODOS_FAILURE';
 
 // Called "Action Creators" - factory function, use these instead of bare actions
-
 export const createTodo = text => {
   return {
     type: CREATE_TODO,
@@ -25,3 +27,17 @@ export const markTodoAsCompleted = text => {
       payload: { text }
   }
 }
+
+export const loadTodosInProgress = () => {
+  type: LOAD_TODOS_IN_PROGRESS
+}
+
+
+export const loadTodosSuccess = todos => ({
+  type: LOAD_TODOS_SUCCESS,
+  payload: { todos }
+});
+
+export const loadTodosFailure = () => ({
+  type: LOAD_TODOS_FAILURE,
+});
--- src/Todos/thunks.js
+++ src/Todos/thunks.js
@@ -1,3 +1,21 @@
+import { loadTodosFailure, loadTodosSuccess } from "./actions";
+import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from './actions';
+
+/*
+
+*/
+export const loadTodos = () => async (dispatch, getState) => {
+  try {
+    dispatch(loadTodosInProgress());
+    const response = await fetch('http://localhost:8080/todos');
+    const todos = await response.json();
+    dispatch(loadTodosSuccess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailure());
+    dispatch(displayAlert(e));
+  }
+}
+
 export const displayAlert = text => {
   alert(`You clicked ${text}!`);
 }
