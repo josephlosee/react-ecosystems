b606623d3eb9b07747ea2fe3de93c7871bad5b9a
Joseph Losee <josephlosee@gmail.com>
2020-11-09 10:01:36 -0800

    Part 4-3: Installed thunk, babel transform for async, and did basic setup to support thunk

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import { todos} from './todos/reducers';
-
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
 const reducers = {todos,};
 
 const persistConfig = {
@@ -16,5 +17,5 @@ const rootReducer = combineReducers(reducers); //package to a type consumable by
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const configureStore = () => createStore(
   persistedReducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(applyMiddleware(thunk))
 );
